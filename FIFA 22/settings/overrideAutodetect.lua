6c13cb59a8d0da0168fb6d67e1561027e74241d3
Ilyas Maghraoui <your-email@gmail.com>
2026-05-20 22:56:57 +0100

    gdfsg

---
+++ FIFA 22/settings/overrideAutodetect.lua
@@ -0,0 +1,16 @@
+FullscreenEnabled = 1	     	   		  	     	       	       
+ResolutionWidth = 1280  	   	   	 	     		    
+ResolutionHeight = 720      			   	  	       
+AntiAliasingDeferred = 1	  	 	      	   	    	  
+OverallGraphicsQuality = 1     				 	   	     
+VSyncEnabled = 1    	     	 	      	  		    	   
+PresentInterval = 1      	  	       	    	 	     	  
+WindowedBorderless = 0  	  	    		  	      	       
+   	   	     		    	      	     	 	   	  
+ 	  	 	       	      	    	   	       	   	     
+     	 	   	    	     	  	  	 	       	     
+      	       	  		    	   	    	    	    	       
+	   	       	    	  	   	  	  	     	     
+      	   	 	  	 			   	    	    
+	
+
